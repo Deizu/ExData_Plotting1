fef50364bc74de4e768a984feb82ca9228b83f27
Deizu <contact@deasrichardson.com>
2015-08-03 15:23:05 -0400

    Adding easy reference instructions to placeholder scripts.

--- plot4.R
+++ plot4.R
@@ -1,2 +1,6 @@
 # https://github.com/Deizu/ExData_Plotting1
 # R Code to construct Plot 4
+
+# Include code for reading in the data
+# Construct the plot 
+# Save the plot to a PNG file, 480 x 480 pixels.
