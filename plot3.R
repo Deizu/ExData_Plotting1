5f0756749d80c37a750617fd1a2c8b3beaefea4f
Deizu <contact@deasrichardson.com>
2015-08-04 12:34:03 -0400

    Plot 3 code done.

--- plot3.R
+++ plot3.R
@@ -4,3 +4,55 @@
 # Include code for reading in the data
 # Construct the plot 
 # Save the plot to a PNG file, 480 x 480 pixels.
+
+# Load required libraries
+library(lubridate)
+library(dplyr)
+
+# Read in the data.
+# if(!exists("./data/")) {dir.create("./data/")}
+# if!(exists("./data/household_power_consumption.txt")) {
+#   
+#   dataurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#   
+#   download.file(dataurl,destfile="./data/data.zip",mode='wb')
+#   
+#   unzip(zipfile="./data/data.zip", files=NULL, list=FALSE, overwrite=FALSE, 
+#         junkpaths=FALSE, exdir=paste(getwd(),"/data",sep=""), unzip="internal",
+#         setTimes=FALSE)
+#   
+#   rm(dataurl)
+# }
+cc <- c("character", "character", "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric")
+
+rawdata <- read.table("./data/household_power_consumption.txt", header=TRUE,
+                      sep=";", na.strings="?", colClasses=cc)
+
+# Subset to grab only the 2 dates of interest.
+
+data <- rbind(subset(rawdata,Date=="1/2/2007"),subset(rawdata,Date=="2/2/2007"))
+
+row.names(data) <- NULL
+
+data$DT <- paste(data$Date, data$Time)
+
+data$DT <- strptime(data$DT,format="%d/%m/%Y %H:%M:%S")
+
+# Convert to date and time formats with lubridate functions
+
+data$Date <- dmy(data$Date)
+data$Time <- hms(data$Time)
+
+# Plot the graph
+
+png(file="plot3.png", width=480, height=480)
+
+with(data, plot(DT, Sub_metering_1, type="l", xlab="", 
+                ylab="Energy sub metering")) 
+with(data, lines(DT, Sub_metering_2, col="red"))
+with(data, lines(DT, Sub_metering_3, col="blue"))
+with(data, legend("topright", lty=1, col=c("black","blue","red"), 
+     legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")))
+
+dev.off()
