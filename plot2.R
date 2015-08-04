1c6e691bfd061297209264f9181af7a5f393cab2
Deizu <contact@deasrichardson.com>
2015-08-04 12:10:59 -0400

    Plot 2 code done.

--- plot2.R
+++ plot2.R
@@ -4,3 +4,51 @@
 # Include code for reading in the data
 # Construct the plot 
 # Save the plot to a PNG file, 480 x 480 pixels.
+
+# Load required libraries
+library(lubridate)
+library(dplyr)
+
+# Read in the data.
+# if(!exists("./data/")) {dir.create("./data/")}
+# if!(exists("./data/household_power_consumption.txt")) {
+#   
+#   dataurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#   
+#   download.file(dataurl,destfile="./data/data.zip",mode='wb')
+#   
+#   unzip(zipfile="./data/data.zip", files=NULL, list=FALSE, overwrite=FALSE, 
+#         junkpaths=FALSE, exdir=paste(getwd(),"/data",sep=""), unzip="internal",
+#         setTimes=FALSE)
+#   
+#   rm(dataurl)
+# }
+cc <- c("character", "character", "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric")
+
+rawdata <- read.table("./data/household_power_consumption.txt", header=TRUE,
+                      sep=";", na.strings="?", colClasses=cc)
+
+# Subset to grab only the 2 dates of interest.
+
+data <- rbind(subset(rawdata,Date=="1/2/2007"),subset(rawdata,Date=="2/2/2007"))
+
+row.names(data) <- NULL
+
+data$DT <- paste(data$Date, data$Time)
+
+data$DT <- strptime(data$DT,format="%d/%m/%Y %H:%M:%S")
+
+# Convert to date and time formats with lubridate functions
+
+data$Date <- dmy(data$Date)
+data$Time <- hms(data$Time)
+
+# Plot the graph
+
+png(file="plot2.png",width=480,height=480)
+
+plot(x=data$DT, y=data$Global_active_power, typ="l", sub="",
+     xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
