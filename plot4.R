720d294847367dff64545792b45147ddeeef3b9c
Deizu <contact@deasrichardson.com>
2015-08-04 13:33:31 -0400

    Plot 4 code is underway but might need some refinement to match the example.

--- plot4.R
+++ plot4.R
@@ -4,3 +4,70 @@
 # Include code for reading in the data
 # Construct the plot 
 # Save the plot to a PNG file, 480 x 480 pixels.
+
+# Load required libraries
+library(lubridate)
+library(dplyr)
+
+# Read in the data.
+# if(!exists("./data/")) {dir.create("./data/")}
+# if!(exists("./data/household_power_consumption.txt")) {
+#   
+#   dataurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+#   
+#   download.file(dataurl,destfile="./data/data.zip",mode='wb')
+#   
+#   unzip(zipfile="./data/data.zip", files=NULL, list=FALSE, overwrite=FALSE, 
+#         junkpaths=FALSE, exdir=paste(getwd(),"/data",sep=""), unzip="internal",
+#         setTimes=FALSE)
+#   
+#   rm(dataurl)
+# }
+cc <- c("character", "character", "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric")
+
+rawdata <- read.table("./data/household_power_consumption.txt", header=TRUE,
+                      sep=";", na.strings="?", colClasses=cc)
+
+# Subset to grab only the 2 dates of interest.
+
+data <- rbind(subset(rawdata,Date=="1/2/2007"),subset(rawdata,Date=="2/2/2007"))
+
+row.names(data) <- NULL
+
+data$datetime <- paste(data$Date, data$Time)
+
+data$datetime <- strptime(data$datetime,format="%d/%m/%Y %H:%M:%S")
+
+# Convert to date and time formats with lubridate functions
+
+data$Date <- dmy(data$Date)
+data$Time <- hms(data$Time)
+
+# Plot the graph
+
+png(file="plot4.png", width=480, height=480)
+
+par(mfrow = c(2,2), pty="s")
+
+with(data, {
+  
+  plot(datetime, Global_active_power, typ="l", sub="",
+       xlab="", ylab="Global Active Power")
+  
+  plot(datetime, Voltage, typ="l", sub="",
+       xlab="datetime", ylab="Voltage")
+  
+  plot(datetime, Sub_metering_1, type="l", xlab="", 
+                   ylab="Energy sub metering") 
+  lines(datetime, Sub_metering_2, col="red")
+  lines(datetime, Sub_metering_3, col="blue")
+  legend("topright", lty=1, fill=FALSE, border=FALSE, col=c("black","blue","red"), 
+                     legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+  plot(datetime, Global_reactive_power, typ="l", xlab="datetime",
+       ylab="Global_reactive_power")
+  }
+)
+
+dev.off()
